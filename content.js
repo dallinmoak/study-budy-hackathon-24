94d2aa113e12cb5c015c99de7ccb95e83a82926a
rag5798 <bgoettman@yahoo.com>
2024-10-19 08:14:54 -0600

    changed bob to alinks

--- content.js
+++ content.js
@@ -5,9 +5,10 @@ console.log(body)
 console.log('Dom Loaded');
 setTimeout(() => {
   console.log('dom loaded .5 seconds ago')
-  let bobthebuilder = document.querySelectorAll(".Grouping-styles__items a");
-  console.log(bobthebuilder);
+  let alinks = document.querySelectorAll(".Grouping-styles__items a");
+  console.log(alinks);
 }, 500)
+
 
 
 
