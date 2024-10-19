969ba9b7ff464c4b9f36f40098a80beb95cfeb1d
dallinmoak <dallinmoak@gmail.com>
2024-10-19 05:38:24 -0600

    more cleansing

--- content-script.js
+++ content-script.js
@@ -1,13 +1,1 @@
-const indexUrl = chrome.runtime.getURL("index.html");
-console.log(indexUrl);
-fetch(indexUrl)
-  .then((response) => response.text())
-  .then((html) => {
-    const container = document.createElement("div");
-    container.innerHTML = html;
-    document.body.appendChild(container);
-  });
-
-// const myIframe = document.createElement("iframe");
-// myIframe.src = indexUrl;
-// document.body.appendChild(myIframe);
+console.log("content-script.js loaded");
